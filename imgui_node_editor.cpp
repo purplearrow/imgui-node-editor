1d0efbb294db8dafb2503c0d329af52f916c9065
purplearrow <purplearrowjhang@gmail.com>
2023-04-25 08:52:54 +0800

    use string instead of const char* (Config.SettingFile)
    pointer will pointed to temp object. use string would be better

--- imgui_node_editor.cpp
+++ imgui_node_editor.cpp
@@ -5787,7 +5787,7 @@ std::string ed::Config::Load()
             LoadSettings(const_cast<char*>(data.data()), UserPointer);
         }
     }
-    else if (SettingsFile)
+    else if (!SettingsFile.empty())
     {
         std::ifstream file(SettingsFile);
         if (file)
@@ -5833,7 +5833,7 @@ bool ed::Config::Save(const std::string& data, SaveReasonFlags flags)
     {
         return SaveSettings(data.c_str(), data.size(), flags, UserPointer);
     }
-    else if (SettingsFile)
+    else if (!SettingsFile.empty())
     {
         std::ofstream settingsFile(SettingsFile);
         if (settingsFile)
